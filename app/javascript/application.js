9e66540716d247adc208eb9956d2f54f444eca8e
Matthew Baxendale <mdbaxendale1@gmail.com>
2023-03-31 11:17:51 +0100

    navbar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import '@hotwired/turbo-rails'
+
+function handleNavBar() {
+    let burger = document.querySelector('.navbar-burger')
+    const menu = document.querySelector('.navbar-menu')
+    const close = document.querySelector('.navbar-close')
+    burger.addEventListener('click', () => menu.classList.remove('hidden'))
+    close.addEventListener('click', () => menu.classList.add('hidden'))
+}
+
+document.addEventListener('turbo:load', handleNavBar)
